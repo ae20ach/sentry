fe77b0a7f7ad4a1f2df76e18cad86f169708ad17
nsdeschenes <nicholas.deschenes@sentry.io>
2026-04-08 13:12:42 -0300

    Create tooltip wrapper component

--- static/app/components/explore/disabledTraceLink.tsx
+++ static/app/components/explore/disabledTraceLink.tsx
@@ -2,7 +2,7 @@ import type {LocationDescriptorObject} from 'history';
 
 import {Link} from '@sentry/scraps/link';
 import {Text} from '@sentry/scraps/text';
-import {Tooltip} from '@sentry/scraps/tooltip';
+import {Tooltip, type TooltipProps} from '@sentry/scraps/tooltip';
 
 import {t, tct} from 'sentry/locale';
 
@@ -10,6 +10,45 @@ interface DisabledTraceLinkProps {
   children: React.ReactNode;
   type: 'trace' | 'span';
   similarEventsUrl?: LocationDescriptorObject | string;
+}
+
+interface DisabledTraceLinkTooltipProps extends Omit<TooltipProps, 'title'> {
+  type: DisabledTraceLinkProps['type'];
+  similarEventsUrl?: DisabledTraceLinkProps['similarEventsUrl'];
+}
+
+export function DisabledTraceLinkTooltip({
+  children,
+  type,
+  similarEventsUrl,
+  ...tooltipProps
+}: DisabledTraceLinkTooltipProps) {
+  const title =
+    type === 'trace' ? (
+      similarEventsUrl ? (
+        <Text>
+          {tct('Trace is older than 30 days. [similarLink] in the past 24 hours.', {
+            similarLink: <Link to={similarEventsUrl}>{t('View similar traces')}</Link>,
+          })}
+        </Text>
+      ) : (
+        <Text>{t('Trace is older than 30 days')}</Text>
+      )
+    ) : similarEventsUrl ? (
+      <Text>
+        {tct('Span is older than 30 days. [similarLink] in the past 24 hours.', {
+          similarLink: <Link to={similarEventsUrl}>{t('View similar spans')}</Link>,
+        })}
+      </Text>
+    ) : (
+      <Text>{t('Span is older than 30 days')}</Text>
+    );
+
+  return (
+    <Tooltip showUnderline isHoverable {...tooltipProps} title={title}>
+      {children}
+    </Tooltip>
+  );
 }
 
 /**
@@ -23,35 +62,11 @@ export function DisabledTraceLink({
   type,
   similarEventsUrl,
 }: DisabledTraceLinkProps) {
-  let tooltipContent: React.ReactNode;
-
-  if (type === 'trace') {
-    tooltipContent = similarEventsUrl ? (
-      <Text>
-        {tct('Trace is older than 30 days. [similarLink] in the past 24 hours.', {
-          similarLink: <Link to={similarEventsUrl}>{t('View similar traces')}</Link>,
-        })}
-      </Text>
-    ) : (
-      <Text>{t('Trace is older than 30 days')}</Text>
-    );
-  } else {
-    tooltipContent = similarEventsUrl ? (
-      <Text>
-        {tct('Span is older than 30 days. [similarLink] in the past 24 hours.', {
-          similarLink: <Link to={similarEventsUrl}>{t('View similar spans')}</Link>,
-        })}
-      </Text>
-    ) : (
-      <Text>{t('Span is older than 30 days')}</Text>
-    );
-  }
-
   return (
-    <Tooltip showUnderline isHoverable title={tooltipContent}>
+    <DisabledTraceLinkTooltip type={type} similarEventsUrl={similarEventsUrl}>
       <Text variant="muted" aria-disabled="true" role="link">
         {children}
       </Text>
-    </Tooltip>
+    </DisabledTraceLinkTooltip>
   );
 }
