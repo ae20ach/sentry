02c239b95c9f80b41487279b3ed7709b6f687296
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-04-10 17:20:01 -0700

    empty screen ui elements

--- static/app/views/settings/organizationRepositories/index.tsx
+++ static/app/views/settings/organizationRepositories/index.tsx
@@ -1,12 +1,17 @@
 import {useMemo} from 'react';
 import {useQuery} from '@tanstack/react-query';
 
-import {Flex, Stack} from '@sentry/scraps/layout';
+import {Button, ButtonBar} from '@sentry/scraps/button';
+import {Container, Flex, Stack} from '@sentry/scraps/layout';
 import {ExternalLink} from '@sentry/scraps/link';
+import {SegmentedControl} from '@sentry/scraps/segmentedControl';
+import {Text} from '@sentry/scraps/text';
 
 import {hasEveryAccess} from 'sentry/components/acl/access';
 import {AnalyticsArea} from 'sentry/components/analyticsArea';
+import {DropdownButton} from 'sentry/components/dropdownButton';
 import {DropdownMenu} from 'sentry/components/dropdownMenu';
+import {EmptyMessage} from 'sentry/components/emptyMessage';
 import {RepoProviderIcon} from 'sentry/components/repositories/repoProviderIcon';
 import {organizationConfigIntegrationsQueryOptions} from 'sentry/components/repositories/scmIntegrationTree/organizationConfigIntegrationsQueryOptions';
 import {ScmIntegrationTree} from 'sentry/components/repositories/scmIntegrationTree/scmIntegrationTree';
@@ -14,6 +19,11 @@ import {ScmTreeFilters} from 'sentry/components/repositories/scmIntegrationTree/
 import {useScmTreeFilters} from 'sentry/components/repositories/scmIntegrationTree/useScmTreeFilters';
 import {SentryDocumentTitle} from 'sentry/components/sentryDocumentTitle';
 import {IconAdd} from 'sentry/icons/iconAdd';
+import {IconBitbucket} from 'sentry/icons/iconBitbucket';
+import {IconChevron} from 'sentry/icons/iconChevron';
+import {IconGithub} from 'sentry/icons/iconGithub';
+import {IconGitlab} from 'sentry/icons/iconGitlab';
+import {IconVsts} from 'sentry/icons/iconVsts';
 import {t, tct} from 'sentry/locale';
 import {isActiveSuperuser} from 'sentry/utils/isActiveSuperuser';
 import {useOrganization} from 'sentry/utils/useOrganization';
@@ -36,6 +46,7 @@ export default function OrganizationRepositories() {
     [providersQuery.data]
   );
 
+  // console.log({scmProviders});
   // const {startFlow} = useAddIntegration({
   //   provider,
   //   organization,
@@ -44,6 +55,7 @@ export default function OrganizationRepositories() {
   //   modalParams,
   // });
 
+  const hasProviders = false; // scmProviders.length > 0;
   const {repoFilter, setRepoFilter, searchTerm, setSearchTerm} = useScmTreeFilters();
 
   return (
@@ -52,7 +64,7 @@ export default function OrganizationRepositories() {
       <SettingsPageHeader
         title={t('Repositories')}
         action={
-          organization.features.includes('scm-trimmed-tree') ? (
+          organization.features.includes('scm-trimmed-tree') && hasProviders ? (
             <DropdownMenu
               size="sm"
               triggerLabel={t('Connect Source Code')}
@@ -75,43 +87,183 @@ export default function OrganizationRepositories() {
             />
           ) : null
         }
-        subtitle={tct(
-          'Integrate with a [scm:Source Code Management] provider and then connect repositories with Sentry. Connecting a repo to a project enables [suspect_commits:Suspect Commits] on issues, [suggested_assignees:Suggested Assignees] based on code owners, the ability to mark an issue [resolved_via_commit:Resolved via Commit or PR], and is a requirement for [seer:Seer].',
-          {
-            scm: (
-              <ExternalLink href="https://docs.sentry.io/organization/getting-started/#source-code-management" />
-            ),
-            suspect_commits: (
-              <ExternalLink href="https://docs.sentry.io/product/issues/suspect-commits/" />
-            ),
-            suggested_assignees: (
-              <ExternalLink href="https://docs.sentry.io/product/issues/ownership-rules/#code-owners" />
-            ),
-            resolved_via_commit: (
-              <ExternalLink href="https://docs.sentry.io/product/releases/associate-commits/#associate-commits-with-a-release" />
-            ),
-            seer: (
-              <ExternalLink href="https://docs.sentry.io/product/ai-in-sentry/seer/#seer-capabilities" />
-            ),
-          }
-        )}
+        subtitle={
+          hasProviders
+            ? tct(
+                'Integrate with a [scm:Source Code Management] provider and then connect repositories with Sentry. Connecting a repo to a project enables [suspect_commits:Suspect Commits] on issues, [suggested_assignees:Suggested Assignees] based on code owners, the ability to mark an issue [resolved_via_commit:Resolved via Commit or PR], and is a requirement for [seer:Seer].',
+                {
+                  scm: (
+                    <ExternalLink href="https://docs.sentry.io/organization/getting-started/#source-code-management" />
+                  ),
+                  suspect_commits: (
+                    <ExternalLink href="https://docs.sentry.io/product/issues/suspect-commits/" />
+                  ),
+                  suggested_assignees: (
+                    <ExternalLink href="https://docs.sentry.io/product/issues/ownership-rules/#code-owners" />
+                  ),
+                  resolved_via_commit: (
+                    <ExternalLink href="https://docs.sentry.io/product/releases/associate-commits/#associate-commits-with-a-release" />
+                  ),
+                  seer: (
+                    <ExternalLink href="https://docs.sentry.io/product/ai-in-sentry/seer/#seer-capabilities" />
+                  ),
+                }
+              )
+            : null
+        }
       />
-      <Stack gap="lg">
-        <Flex align="center" gap="md">
-          <ScmTreeFilters
+      {hasProviders ? (
+        <Stack gap="lg">
+          <Flex align="center" gap="md">
+            <ScmTreeFilters
+              repoFilter={repoFilter}
+              setRepoFilter={setRepoFilter}
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+            />
+          </Flex>
+          <ScmIntegrationTree
+            providerFilter="all"
             repoFilter={repoFilter}
-            setRepoFilter={setRepoFilter}
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
+            search={searchTerm}
+            showEmptyProviders={!organization.features.includes('scm-trimmed-tree')}
           />
-        </Flex>
-        <ScmIntegrationTree
-          providerFilter="all"
-          repoFilter={repoFilter}
-          search={searchTerm}
-          showEmptyProviders={!organization.features.includes('scm-trimmed-tree')}
-        />
-      </Stack>
+        </Stack>
+      ) : (
+        <Container border="secondary" radius="xl" padding="3xl">
+          <Stack gap="2xl" maxWidth="668px" justifySelf="center" padding="xl 0">
+            <Text align="center">
+              {tct(
+                'Connect your source code providers, manage repositories, and configure code mappings to enable [suspect:Suspect Commits], [suggested:Suggested Assignees], and [seer:Seer].',
+                {
+                  suspect: (
+                    <ExternalLink href="https://docs.sentry.io/product/issues/suspect-commits/" />
+                  ),
+                  suggested: (
+                    <ExternalLink href="https://docs.sentry.io/product/issues/ownership-rules/#code-owners" />
+                  ),
+                  seer: (
+                    <ExternalLink href="https://docs.sentry.io/product/ai-in-sentry/seer/#seer-capabilities" />
+                  ),
+                }
+              )}
+            </Text>
+
+            <Flex align="center" gap="md" justify="center">
+              {scmProviders.some(provider =>
+                ['github', 'github_enterprise'].includes(provider.key)
+              ) && (
+                <ButtonBar>
+                  <Button
+                    size="sm"
+                    icon={<IconGithub />}
+                    onClick={() => {
+                      console.log('need to install integration for', 'github');
+                    }}
+                  >
+                    GitHub
+                  </Button>
+                  <DropdownMenu
+                    size="sm"
+                    trigger={(triggerProps, isOpen) => (
+                      <Button
+                        {...triggerProps}
+                        aria-label={t('More options')}
+                        size="sm"
+                        icon={
+                          <IconChevron
+                            variant="muted"
+                            direction={isOpen ? 'up' : 'down'}
+                            size="xs"
+                          />
+                        }
+                        disabled={!canAccess}
+                      />
+                    )}
+                    items={
+                      scmProviders
+                        .filter(provider => provider.key === 'github_enterprise')
+                        .map(provider => ({
+                          key: provider.key,
+                          label: provider.name,
+                          onAction: () => {
+                            console.log('need to install integration for', provider.key);
+                          },
+                        })) ?? []
+                    }
+                  />
+                </ButtonBar>
+              )}
+              {scmProviders.some(provider => provider.key === 'gitlab') && (
+                <Button
+                  size="sm"
+                  icon={<IconGitlab />}
+                  onClick={() => {
+                    console.log('need to install integration for', 'gitlab');
+                  }}
+                >
+                  {t('GitLab')}
+                </Button>
+              )}
+              {scmProviders.some(provider =>
+                ['bitbucket', 'bitbucket_server'].includes(provider.key)
+              ) && (
+                <ButtonBar>
+                  <Button
+                    size="sm"
+                    icon={<IconBitbucket />}
+                    onClick={() => {
+                      console.log('need to install integration for', 'bitbucket');
+                    }}
+                  >
+                    Bitbucket
+                  </Button>
+                  <DropdownMenu
+                    size="sm"
+                    trigger={(triggerProps, isOpen) => (
+                      <Button
+                        {...triggerProps}
+                        aria-label={t('More options')}
+                        size="sm"
+                        icon={
+                          <IconChevron
+                            variant="muted"
+                            direction={isOpen ? 'up' : 'down'}
+                            size="xs"
+                          />
+                        }
+                        disabled={!canAccess}
+                      />
+                    )}
+                    items={
+                      scmProviders
+                        .filter(provider => provider.key === 'bitbucket_server')
+                        .map(provider => ({
+                          key: provider.key,
+                          label: provider.name,
+                          onAction: () => {
+                            console.log('need to install integration for', provider.key);
+                          },
+                        })) ?? []
+                    }
+                  />
+                </ButtonBar>
+              )}
+              {scmProviders.some(provider => provider.key === 'vsts') && (
+                <Button
+                  size="sm"
+                  icon={<IconVsts />}
+                  onClick={() => {
+                    console.log('need to install integration for', 'vsts');
+                  }}
+                >
+                  {t('Azure')}
+                </Button>
+              )}
+            </Flex>
+          </Stack>
+        </Container>
+      )}
     </AnalyticsArea>
   );
 }
