d1a3e6c2ee4ee44c8da89ca08703ee06f4be51d5
Hector <hector.dearman@sentry.io>
2026-04-20 14:33:59 +0100

    DNM: No op change

--- tests/acceptance/test_accept_organization_invite.py
+++ tests/acceptance/test_accept_organization_invite.py
@@ -10,6 +10,7 @@ from sentry.testutils.silo import no_silo_test
 
 # When we want to set this @cell_silo_test, we'll need to configure regions in order for invites to work.
 # See the accept_organization_invite.py#get_invite_state logic
+# Some change
 @no_silo_test
 class AcceptOrganizationInviteTest(AcceptanceTestCase):
     def setUp(self) -> None:
