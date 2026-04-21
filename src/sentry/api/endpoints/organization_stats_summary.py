8286d53465183c4f608bb13aff77762c190f2c13
junior <junior@users.noreply.github.com>
2026-04-21 11:37:54 +0000

    fix(api-docs): correct stats summary param description spacing

--- src/sentry/api/endpoints/organization_stats_summary.py
+++ src/sentry/api/endpoints/organization_stats_summary.py
@@ -62,7 +62,7 @@ class OrgStatsSummaryQueryParamsSerializer(serializers.Serializer):
     )
     end = serializers.DateTimeField(
         help_text=(
-            "This defines the inclusive end of the time series range as an explicit datetime, either in UTC ISO8601 or epoch seconds."
+            "This defines the inclusive end of the time series range as an explicit datetime, either in UTC ISO8601 or epoch seconds. "
             "Use along with `start` instead of `statsPeriod`."
         ),
         required=False,
