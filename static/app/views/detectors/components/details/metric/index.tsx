1cf9afa9919f47d4b346ae6f5712238c4262e903
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2026-04-21 14:04:34 -0700

    fix issue where statsPeriod could be set lower than we can access, redirect to smallest avialable window instead

--- static/app/views/detectors/components/details/metric/index.tsx
+++ static/app/views/detectors/components/details/metric/index.tsx
@@ -18,6 +18,7 @@ import {
 import {useIsMigratedExtrapolation} from 'sentry/views/detectors/components/details/metric/utils/useIsMigratedExtrapolation';
 import {getDetectorDataset} from 'sentry/views/detectors/datasetConfig/getDetectorDataset';
 import {DetectorDataset} from 'sentry/views/detectors/datasetConfig/types';
+import {useDetectorStatsPeriods} from 'sentry/views/detectors/hooks/useDetectorStatsPeriods';
 
 type MetricDetectorDetailsProps = {
   detector: MetricDetector;
@@ -39,6 +40,7 @@ export function MetricDetectorDetails({detector, project}: MetricDetectorDetails
   });
 
   const intervalSeconds = dataSource.queryObj?.snubaQuery.timeWindow;
+  useDetectorStatsPeriods(interval);
 
   return (
     <DetailLayout>
