a16e7a8103ef05ad966a6e4bf82e57db8f84ed99
Josh Goldberg ✨ <joshua.goldberg@sentry.io>
2026-04-21 14:39:34 -0400

    bump up to 10k

--- static/app/views/replays/detail/network/constants.ts
+++ static/app/views/replays/detail/network/constants.ts
@@ -1,5 +1,4 @@
 /**
  * How many replay network requests to show in one paginated page.
  */
-export const NETWORK_LIST_PAGE_SIZE = 100;
-// export const NETWORK_LIST_PAGE_SIZE = 10_000;
+export const NETWORK_LIST_PAGE_SIZE = 10_000;
